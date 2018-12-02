8aaad18edd61ff6222d3dc3e6e89e7aad5f8aacd
Timi <1229144339@qq.com>
2018-12-02 18:33:11 +0800

    add the downloadController

---
+++ src/main/java/com/zhanchormanage/controller/DownLoadController.java
@@ -0,0 +1,72 @@
+package com.zhanchormanage.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+
+@Controller
+public class DownLoadController {
+    @RequestMapping(value = "/fileDownLoad_artiste_supply.action", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> fileDownLoadArtisteSupply(String fileName) throws Exception {
+        String path = "C://AristeSupplyContract/" + fileName;
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        // String fileName=new
+        // String("你好.xlsx".getBytes("UTF-8"),"iso-8859-1");//为了解决中文名称乱码问题
+        String fileNameToUTF8 = new String(file.getName().getBytes("UTF-8"), "iso-8859-1");// 为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileNameToUTF8);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/fileDownLoad_artist_brokerage.action",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> fileDownLoadArtistBrokerage(String fileName) throws Exception {
+        String path = "C://ArtistBrokerageContract/"+fileName;
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        // String fileName=new
+        // String("你好.xlsx".getBytes("UTF-8"),"iso-8859-1");//为了解决中文名称乱码问题
+        String fileNameToUTF8 = new String(file.getName().getBytes("UTF-8"), "iso-8859-1");// 为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileNameToUTF8);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/fileDownLoad_commentator.action",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> fileDownLoadCommentator(String fileName) throws Exception {
+        String path = "C://CommentatorContract/"+fileName;
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        // String fileName=new
+        // String("你好.xlsx".getBytes("UTF-8"),"iso-8859-1");//为了解决中文名称乱码问题
+        String fileNameToUTF8 = new String(file.getName().getBytes("UTF-8"), "iso-8859-1");// 为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileNameToUTF8);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/fileDownLoad_three_party_contract.action",method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> fileDownLoadThreePartyContract(String fileName) throws Exception {
+        String path = "C://ThreePartyContract/"+fileName;
+        File file = new File(path);
+        HttpHeaders headers = new HttpHeaders();
+        // String fileName=new
+        // String("你好.xlsx".getBytes("UTF-8"),"iso-8859-1");//为了解决中文名称乱码问题
+        String fileNameToUTF8 = new String(file.getName().getBytes("UTF-8"), "iso-8859-1");// 为了解决中文名称乱码问题
+        headers.setContentDispositionFormData("attachment", fileNameToUTF8);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+}
+
+
